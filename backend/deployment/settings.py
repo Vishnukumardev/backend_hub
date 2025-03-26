74553d1522e3866150db4414be2690c697d73f31
Vishnu Kumar <vishnusoftwaredev@gmail.com>
2025-03-27 00:05:58 +0530

    changed inner project name to deployment - VK -  #7

--- backend/deployment/settings.py
+++ backend/deployment/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'deployment.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'deployment.wsgi.application'
 
 
 # Database
